4ee208e279460d6851e6bd99d8dc8a0a910f8b9f
Malcolm Inglis <me@minglis.id.au>
2012-09-24 17:24:28 +1000

    Complete triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,11 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  case [a,b,c].uniq.length
-  when 1 then :equilateral
-  when 2 then :isosceles
-  else :scalene
-  end
+  # sides = [a,b,c].sort
+  # if sides.count { |x| x <= 0 } > 0
+  #   raise TriangleError, "Invalid side lengths: #{sides}"
+  # elsif sides[0] + sides[1] <= sides[2]
+  #   raise TriangleError, "Incompatible side lengths: #{sides}"
+  # end
+  # case sides.uniq.length
+  # when 1 then :equilateral
+  # when 2 then :isosceles
+  # else :scalene
+  # end
+
+  # Better answer from stack overflow: (slightly modified)
+  a,b,c = [a,b,c].sort
+  raise TriangleError, 'Invalid side lengths' if a + b <= c
+  [:equilateral, :isosceles, :scalene].fetch([a,b,c].uniq.size - 1)
 end
 
 # Error class used in part 2.  No need to change this code.
