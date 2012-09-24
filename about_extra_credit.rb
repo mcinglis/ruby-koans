6ddce7c6ac52ef4e9cd157734905cfede2b2cfdb
Malcolm Inglis <me@minglis.id.au>
2012-09-25 02:21:29 +1000

    Copy functions over to extra credit

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -6,3 +6,22 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+def roll(n)
+  (1..6).to_a.sample(n)
+end
+
+def score(dice)
+  (1..6).map do |result|
+    count = dice.count(result)
+    case result
+    when 1 then (1000 * (count / 3)) + (100 * (count % 3))
+    when 5 then (500 * (count / 3)) + (50 * (count % 3))
+    else 100 * result * (count / 3)
+    end
+  end.reduce(:+)
+end
+
+def play(n)
+end
+
